9e863727e3f6756864db526ec39f080e806fc0b8
zq-zhan <2510031203@qq.com>
2024-11-29 12:02:43 +0800

    自写20241129

--- 自写练习记录/20241113滑动窗口.py
+++ 自写练习记录/20241113滑动窗口.py
@@ -693,5 +693,19 @@ class Solution1():
 		return ans
 
 
-
+# 每种字符至少取K个
+## 方法一：自写版，逆向思维
+class Solution1():
+	def takeCharacters(self,s,k):
+		ans=left=0
+		dic_all=Counter(s)
+		dic_win=Counter()
+		for right,c in enumerate(s):
+			dic_win[c]+=1
+			# while dic_all['a']-dic_win['a']<k or dic_all['b']-dic_win['b']<k or dic_all['c']-dic_win['c']<k:
+			whiel dic_all[c]-dic_win[c]<k:
+				dic_win[s[left]]-=1
+				left += 1
+			ans=max(ans,right-left+1)
+		return len(s)-ans
 
